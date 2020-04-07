6a8c85ab7e53c1fa4fb82ad1be11845fb11f3cac
Ageu Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2020-04-07 10:54:35 -0300

    added new files

---
+++ autoexecbot.bat
@@ -0,0 +1,16 @@
+c:
+cd\Users\agl_r\Documents\bots
+echo "iniciando bots"
+python startbots.py
+echo "iniciando bots facebook"
+python startfacebook.py
+echo "iniciando bots instagram"
+python startinstagram.py
+echo "iniciando bots linkedin"
+python startlinkedin.py
+echo "iniciando bots telegram"
+python starttelegram.py
+echo "iniciando bots twitter"
+python starttwitter.py
+echo "iniciando bots whatsapp"
+python startwhatsapp.py
